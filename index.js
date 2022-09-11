195e88a578ec08ace9c8ee33d68c3efb6aa6889d
stuartambient <sambient@gmail.com>
2022-09-11 17:27:51 -0600

    still working through the breakpoints

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 const open = document.querySelector('.navtop__open');
 const close = document.querySelector('.navtop__close');
+const mobileMenu = document.querySelector('.mobile__menu');
 
 open.addEventListener('click', () => {
   open.classList.toggle('close-menu');
   close.classList.toggle('open-menu');
+  mobileMenu.classList.toggle('mobile__menu-open');
 });
 
 close.addEventListener('click', () => {
   open.classList.toggle('close-menu');
   close.classList.toggle('open-menu');
+  mobileMenu.classList.toggle('mobile__menu-open');
 });
